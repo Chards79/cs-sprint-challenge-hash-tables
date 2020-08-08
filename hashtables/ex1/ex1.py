e2e66b4bba3d2c630d30c97a43953306f0dda01c
Eric Richards <esholundr@yahoo.com>
2020-08-08 14:23:49 -0600

    added new code to ex1, not passing tests yet

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,12 +3,24 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
+    HashTable = {}
 
-    return None
+    for x in weights:
+        if x is not None:
+            HashTable[x] =  # the list index
+
+    Arr = []
+
+    for x in HashTable:
+        if limit == x + x:
+            Arr.append(x)
+            return Arr
+        else:
+            return None
 
 
 # need to be able to find 2 weights in a list that add up to the given limit
 # need to return the indices of those 2 weights in a tuple, or None if there is no combination that adds up to the limit
 # probably want to iterate through the list of weights
-# want to store each weight as a key with a value of its list index
+# want to store each weight as a key with a value of its list index inside of a hash table/dictionary
 #
